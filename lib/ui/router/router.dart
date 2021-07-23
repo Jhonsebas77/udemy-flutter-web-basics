70055755578af3afc6d4948431e1f761e63a487f
Sebastian Otalora <sebastianotalora@tyba.com>
2021-07-23 09:44:05 -0500

    Add the navigation to other pages

--- lib/ui/router/router.dart
+++ lib/ui/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:fluro/fluro.dart';
+import 'package:flutter_web_basics/ui/views/counter_view_provider.dart';
 import 'package:flutter_web_basics/ui/views/counter_view_statefull.dart';
 
 class Flurorouter {
@@ -8,10 +9,29 @@ class Flurorouter {
     router.define(
       '/',
       handler: _counterHandler,
+      transitionType: TransitionType.fadeIn,
+    );
+    router.define(
+      '/statefull',
+      handler: _stateFullCounterHandler,
+      transitionType: TransitionType.fadeIn,
+    );
+    router.define(
+      '/provider',
+      handler: _providerCounterHandler,
+      transitionType: TransitionType.fadeIn,
     );
   }
 
   static Handler _counterHandler = Handler(
     handlerFunc: (context, params) => CounterViewStateFull(),
+  );
+
+  static Handler _stateFullCounterHandler = Handler(
+    handlerFunc: (context, params) => CounterViewStateFull(),
+  );
+
+  static Handler _providerCounterHandler = Handler(
+    handlerFunc: (context, params) => CounterProviderView(),
   );
 }
